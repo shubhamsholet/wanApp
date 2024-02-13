e7488ac5b1fab8fe55c4e16cf9b48c42f7b9cb5c
Shubham Sholet <shubham.sholet@paavu.com>
2024-02-13 18:12:36 +0530

    pipe and services created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,12 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { SearchPipePipe } from './pipes/search-pipe.pipe';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, SearchPipePipe],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
