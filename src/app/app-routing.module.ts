d6cc5cd1d02a482846a04588af6f41f52ecd8a53
Shubham Sholet <shubham.sholet@paavu.com>
2024-02-13 10:52:04 +0530

    pages folder

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,52 +9,53 @@ const routes: Routes = [
   },
   {
     path: 'folder/:id',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+    loadChildren: () => import('./folder/folder.module').then(m => m.FolderPageModule)
   },
   {
     path: 'videos',
-    loadChildren: () => import('./pages/videos/videos.module').then( m => m.VideosPageModule)
+    loadChildren: () => import('./pages/videos/videos.module').then(m => m.VideosPageModule)
   },
   {
     path: 'profile',
-    loadChildren: () => import('./pages/profile/profile.module').then( m => m.ProfilePageModule)
+    loadChildren: () => import('./pages/profile/profile.module').then(m => m.ProfilePageModule)
   },
   {
     path: 'users',
-    loadChildren: () => import('./pages/users/users.module').then( m => m.UsersPageModule)
+    loadChildren: () => import('./pages/users/users.module').then(m => m.UsersPageModule)
   },
   {
     path: 'playout',
-    loadChildren: () => import('./pages/playout/playout.module').then( m => m.PlayoutPageModule)
+    loadChildren: () => import('./pages/playout/playout.module').then(m => m.PlayoutPageModule)
   },
   {
     path: 'invoice',
-    loadChildren: () => import('./pages/invoice/invoice.module').then( m => m.InvoicePageModule)
+    loadChildren: () => import('./pages/invoice/invoice.module').then(m => m.InvoicePageModule)
   },
   {
     path: 'inventory',
-    loadChildren: () => import('./pages/inventory/inventory.module').then( m => m.InventoryPageModule)
+    loadChildren: () => import('./pages/inventory/inventory.module').then(m => m.InventoryPageModule)
   },
   {
     path: 'campaign',
-    loadChildren: () => import('./pages/campaign/campaign.module').then( m => m.CampaignPageModule)
+    loadChildren: () => import('./pages/campaign/campaign.module').then(m => m.CampaignPageModule)
   },
   {
     path: 'location',
-    loadChildren: () => import('./pages/location/location.module').then( m => m.LocationPageModule)
+    loadChildren: () => import('./pages/location/location.module').then(m => m.LocationPageModule)
   },
   {
     path: 'category',
-    loadChildren: () => import('./pages/category/category.module').then( m => m.CategoryPageModule)
+    loadChildren: () => import('./pages/category/category.module').then(m => m.CategoryPageModule)
   },
   {
     path: 'playlists',
-    loadChildren: () => import('./pages/playlists/playlists.module').then( m => m.PlaylistsPageModule)
+    loadChildren: () => import('./pages/playlists/playlists.module').then(m => m.PlaylistsPageModule)
   },
   {
     path: 'devices',
-    loadChildren: () => import('./pages/devices/devices.module').then( m => m.DevicesPageModule)
-  }
+    loadChildren: () => import('./pages/devices/devices.module').then(m => m.DevicesPageModule)
+  },
+
 ];
 
 @NgModule({
@@ -63,4 +64,4 @@ const routes: Routes = [
   ],
   exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
