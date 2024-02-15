2bc671dfbc91ec60590225287db6f83645d6378e
Shubham Sholet <shubham.sholet@paavu.com>
2024-02-15 12:15:45 +0530

    linked page material

--- src/app/pages/devices/linked/linked.page.ts
+++ src/app/pages/devices/linked/linked.page.ts
@@ -7,6 +7,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LinkedPage implements OnInit {
 
+  divicesdetails: any = [
+    {
+      divicename: 'manas hospital',
+      location: 'delhi',
+      videoscount: 10,
+      edit: true,
+    },
+    {
+      divicename: 'parveen hospital',
+      location: 'mumbai',
+      videoscount: 110,
+      edit: false,
+    },
+    {
+      divicename: 'ravindra hospital',
+      location: 'gurgaun',
+      videoscount: 20,
+      edit: true,
+    },
+    {
+      divicename: 'paras hospital',
+      location: 'delhi',
+      videoscount: 110,
+      edit: false,
+    },
+    {
+      divicename: 'rohit clinic',
+      location: 'bawal',
+      videoscount: 1,
+      edit: true,
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
